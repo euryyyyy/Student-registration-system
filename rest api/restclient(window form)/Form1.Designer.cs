0f55e17e01f1c17c890edc47d58203a23d8690f0
leqc <144880822+euryyyyy@users.noreply.github.com>
2026-04-17 22:45:30 +0800

    Add files via upload

---
+++ rest api/restclient(window form)/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace StudentClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(140, 95);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(128, 22);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(140, 141);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(128, 22);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(153, 185);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(86, 23);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(66, 185);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(65, 214);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(126, 23);
+            this.btnView.TabIndex = 6;
+            this.btnView.Text = "View Users";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click_1);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(65, 243);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(145, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update Password";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(65, 272);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(145, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete User";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.ItemHeight = 16;
+            this.lstUsers.Location = new System.Drawing.Point(66, 332);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(311, 324);
+            this.lstUsers.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 712);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox lstUsers;
+    }
+}
+
---
+++ rest api/restclient(window form)/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+namespace StudentClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ================= REGISTER =================
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            using (var client = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["username"] = username;
+                data["password"] = password;
+
+                byte[] response = client.UploadValues(
+                    "http://localhost/student-api/api.php?action=register",
+                    "POST",
+                    data
+                );
+
+                string result = Encoding.UTF8.GetString(response);
+                MessageBox.Show(result);
+            }
+        }
+
+       
+        // ================= VIEW USERS =================
+        private void btnView_Click_1(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                string url = "http://localhost/student-api/api.php?action=view";
+
+                string json = client.DownloadString(url);
+
+                var users = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(json);
+
+                lstUsers.Items.Clear();
+
+                foreach (var user in users)
+                {
+                    lstUsers.Items.Add(user.username + " - " + user.password);
+                }
+            }
+        }
+        // ================= UPDATE PASSWORD =================
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                var data = new NameValueCollection();
+
+                data["username"] = txtUsername.Text;
+                data["password"] = txtPassword.Text;
+
+                byte[] response = client.UploadValues(
+                    "http://localhost/student-api/api.php?action=update",
+                    "POST",
+                    data
+                );
+
+                string result = Encoding.UTF8.GetString(response);
+                MessageBox.Show(result);
+            }
+        }
+
+        // ================= DELETE USER =================
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                var data = new NameValueCollection();
+
+                data["username"] = txtUsername.Text;
+
+                byte[] response = client.UploadValues(
+                    "http://localhost/student-api/api.php?action=delete",
+                    "POST",
+                    data
+                );
+
+                string result = Encoding.UTF8.GetString(response);
+                MessageBox.Show(result);
+            }
+        }
+        // ================= LOGIN =================
+        private void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                var data = new NameValueCollection();
+
+                data["username"] = txtUsername.Text;
+                data["password"] = txtPassword.Text;
+
+                byte[] response = client.UploadValues(
+                    "http://localhost/student-api/api.php?action=login",
+                    "POST",
+                    data
+                );
+
+                string result = Encoding.UTF8.GetString(response);
+                MessageBox.Show(result);
+            }
+        }
+    }
+
+    // ================= USER CLASS =================
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
